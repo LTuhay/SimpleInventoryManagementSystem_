54ecf64a79b537b3bf4b6667f96530bd911770ae
Lorena Tuhay <lorena.tuhay@gmail.com>
2024-06-28 13:53:51 -0300

    Some refactor in product class properties

--- ProductManagement/Product.cs
+++ ProductManagement/Product.cs
@@ -1,30 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace SimpleInventoryManagementSystem.ProductManagement
 {
     public class Product
     {
-        private int id;
-        private string name;
-        private double price;
-        private int quantity;
 
-        public int Id    { get { return id; } set { id = value; } }
-        public string Name { get { return name; } set {  name = value; } }
+        public int Id    { get; set; }
+        public string Name { get; set; }
 
         public double Price { get; set; }
 
         public int Quantity { get; private set; }
 
-        public Product(int Id, string Name, double Price)
+        public Product(int id, string name, double price)
         {
-            this.id = Id;
-            this.name = Name;
-            this.Price = Price;
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
         }
 
         public virtual void IncreaseQuantity (int quantityToIncrease)
